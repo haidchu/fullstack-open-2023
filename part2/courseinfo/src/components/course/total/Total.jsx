16c9a01ae2052bd7363a5d04c85092edbe37756b
Chu Hai <duchaichu1303@gmail.com>
2024-01-03 14:50:00 +0200

    added ex 2.2: course information step 7

--- part2/courseinfo/src/components/course/total/Total.jsx
+++ part2/courseinfo/src/components/course/total/Total.jsx
@@ -2,7 +2,7 @@ const Total = (props) => {
     const { parts } = props;
     const total = parts.reduce((sum, part) => sum + part.exercises, 0);
     return (
-        <p>Number of exercises {total}</p>
+        <p style={{fontWeight: "bold"}}>Total of {total} exercises</p>
     );
 };
 
