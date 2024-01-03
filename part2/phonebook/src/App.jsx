6e67cf0f1718ef53e15f22f69cc94d7a90077b84
Chu Hai <duchaichu1303@gmail.com>
2024-01-03 21:28:41 +0200

    added ex 2.9: the phonebook step 4

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,11 +2,14 @@ import { useState } from "react";
 
 const App = () => {
     const [persons, setPersons] = useState([
-        { name: "Arto Hellas", number: "040-123456" },
+        { name: "Arto Hellas", number: "040-123456", id: 1 },
+        { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+        { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+        { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
     ]);
+    const [filtered, setFiltered] = useState([...persons]);
     const [newName, setNewName] = useState("");
     const [newNumber, setNewNumber] = useState("");
-
     const addPerson = (event) => {
         event.preventDefault();
         const personObject = {
@@ -26,9 +29,22 @@ const App = () => {
     const handleNumberChange = (event) => {
         setNewNumber(event.target.value);
     };
+    const handleFilterChange = (event) => {
+        setFiltered(
+            persons.filter((person) =>
+                person.name
+                    .toLowerCase()
+                    .includes(event.target.value.toLowerCase())
+            )
+        );
+    };
     return (
         <div>
-            <h2>Phonebook</h2>
+            <h1>Phonebook</h1>
+            <form>
+                filter shown with <input onChange={handleFilterChange}/>
+            </form>
+            <h2>Add a new</h2>
             <form onSubmit={addPerson}>
                 <div>
                     name: <input value={newName} onChange={handleNameChange} />
@@ -41,9 +57,9 @@ const App = () => {
                     <button type="submit">add</button>
                 </div>
             </form>
-            <h2>Numbers</h2>
+            <h1>Numbers</h1>
             <ul>
-                {persons.map((person) => {
+                {filtered.map((person) => {
                     return (
                         <li key={person.name}>
                             {person.name} {person.number}
