482906f13c880b3572a73d5aa1a18291f36b7a3c
Chu Hai <duchaichu1303@gmail.com>
2024-01-03 12:16:55 +0200

    added ex 1.7: unicafe step 2

--- part1/unicafe/src/components/Statistics.jsx
+++ part1/unicafe/src/components/Statistics.jsx
@@ -1,5 +1,8 @@
 const Statistics = (props) => {
     const { good, neutral, bad } = props;
+    const total = good + bad + neutral;
+    const average = (good - bad) / total || 0;
+    const positive = (good / total) * 100 || 0;
     return (
         <div>
             <h1>statistics</h1>
@@ -7,6 +10,8 @@ const Statistics = (props) => {
             <p>neutral {neutral}</p>
             <p>bad {bad}</p>
             <p>total {good + bad + neutral}</p>
+            <p>average {average}</p>
+            <p>positive {positive} %</p>
         </div>
     );
 };
