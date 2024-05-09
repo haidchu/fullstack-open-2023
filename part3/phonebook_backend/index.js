be8a434aa4243ee1b6db611dfac39d7686fe8514
Hai Chu <duchaichu1303@gmail.com>
2024-05-09 16:30:18 +0300

    add ex 3.4 phonebook backend step 4 add localhost.rest for endpoints testing

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -2,25 +2,25 @@ const express = require("express")
 const app = express()
 
 let phonebooks = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
+    {
+        "id": 1,
+        "name": "Arto Hellas",
+        "number": "040-123456"
     },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
+    {
+        "id": 2,
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523"
     },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
+    {
+        "id": 3,
+        "name": "Dan Abramov",
+        "number": "12-43-234345"
     },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
+    {
+        "id": 4,
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122"
     }
 ]
 
@@ -33,10 +33,21 @@ app.get("/api/persons", (req, res) => {
 });
 
 app.get("/api/persons/:id", (req, res) => {
-    id = parseInt(req.params.id) - 1;
-    if (id > phonebooks.length) res.status(404).send("person not found");
-    res.json(phonebooks[id]);
+    id = parseInt(req.params.id);
+    person = phonebooks.find((item) => item.id === id);
+    if (person) res.status(200).json(person);
+    res.status(404).send("person not found");
 });
+
+app.delete("/api/persons/:id", (req, res) => {
+    id = parseInt(req.params.id);
+    person = phonebooks.find((item) => item.id === id);
+    if (person) {
+        phonebooks = phonebooks.filter((item) => item.id !== person.id);
+        res.status(200).send(`person with id ${id} deleted`);
+    }
+    res.status(404).send("person not found");
+})
 
 app.get("/info", (req, res) => {
     current = new Date().toString()
