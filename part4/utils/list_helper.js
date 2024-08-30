094bf37e3bf52cc579b9807b91dc9f41890f68a9
Hai Chu <haichu@HaiChuPC>
2024-08-30 09:09:51 +0700

    ex 4.4: Helper Functions and Unit Tests, step 2

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -2,6 +2,14 @@ const dummy = (blog) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    count = blogs.reduce((sum, item) => {
+        return sum + item.likes
+    }, 0)
+    return count;
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
