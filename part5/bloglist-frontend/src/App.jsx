0afd35d4fabc5729b03d676542801ec90bead211
Hai Chu <haichu@HaiChuPC>
2024-10-22 23:46:00 +0300

    add ex 5.2: Blog List Frontend, step 2

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -4,70 +4,79 @@ import blogService from './services/blogs'
 import axios from 'axios'
 
 const App = () => {
-  const [blogs, setBlogs] = useState([])
-  const [user, setUser] = useState(null)
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
+	const [blogs, setBlogs] = useState([])
+	const [user, setUser] = useState(null)
+	const [username, setUsername] = useState('')
+	const [password, setPassword] = useState('')
 
-  useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs(blogs)
-    )
-  }, [])
+	useEffect(() => {
+		blogService.getAll().then(blogs =>
+			setBlogs(blogs)
+		)
+	}, [])
 
-  const handleLogin = async (e) => {
-    e.preventDefault()
-    const res = await axios.post('/api/login', {
-      username: username,
-      password: password,
-    }, {
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    })
-    setUser({
-      name: res.data.name,
-      username: res.data.username,
-      token: res.data.token
-    })
-  }
-  if (user === null) {
-    return (
-      <div>
-        <h2>Log in to application</h2>
-        <form
-          onSubmit={handleLogin}>
-          <div>
-            <label>Username:
-              <input
-                type="text"
-                name='username'
-                onChange={(e) => { setUsername(e.target.value) }} />
-            </label>
-            <label>Password:
-              <input
-                type="password"
-                name='password'
-                onChange={(e) => { setPassword(e.target.value) }} />
-            </label>
-          </div>
-          <div>
-            <input type="submit" value='Log in' />
-          </div>
-        </form>
-      </div>
-    )
-  }
+	const handleLogin = async (e) => {
+		e.preventDefault()
+		const res = await axios.post('/api/login', {
+			username: username,
+			password: password,
+		}, {
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		})
+		setUser({
+			name: res.data.name,
+			username: res.data.username,
+			token: res.data.token
+		})
+		localStorage.setItem('user', user)
+	}
 
-  return (
-    <div>
-      <h2>blogs</h2>
-      <p>{user.name} logged in</p>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
-    </div>
-  )
+	const handleLogout = async () => {
+		setUsername('')
+		setPassword('')
+		setUser(null)
+		localStorage.clear()
+	}
+	if (user === null) {
+		return (
+			<div>
+				<h2>Log in to application</h2>
+				<form
+					onSubmit={handleLogin}>
+					<div>
+						<label>Username:
+							<input
+								type="text"
+								name='username'
+								onChange={(e) => { setUsername(e.target.value) }} />
+						</label>
+						<label>Password:
+							<input
+								type="password"
+								name='password'
+								onChange={(e) => { setPassword(e.target.value) }} />
+						</label>
+					</div>
+					<div>
+						<input type="submit" value='Log in' />
+					</div>
+				</form>
+			</div>
+		)
+	}
+
+	return (
+		<div>
+			<h2>blogs</h2>
+			<p>{user.name} logged in</p>
+			<button onClick={handleLogout}>log out</button>
+			{blogs.map(blog =>
+				<Blog key={blog.id} blog={blog} />
+			)}
+		</div>
+	)
 }
 
 export default App
